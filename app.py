b2ef1d8d97d99a6eca69fb0aee32d75898cc8a3d
fatimasyed <ftm.smaw@gmail.com>
2025-08-05 12:30:42 +0400

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, Flask!"
-
+def main():
+    print("Route hit!")
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,5 @@
+let upload_email_list = document.getElementById('upload-button');
+let download_attachments = document.getElementById('download-attachments');
+
+upload_email_list.addEventListener('click', fun1())
+download_attachments.addEventListener('click', fun1())
